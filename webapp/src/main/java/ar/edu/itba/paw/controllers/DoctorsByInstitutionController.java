a780d2c4438a9363fae5f6288ebdc56d34f7b37e
Lucas Carmona <lucarmon@itba.edu.ar>
2016-04-12 19:53:57 -0300

    @PathVariable added to DoctorsByInstitutionController and InstitutionDoctorAppointmentController. ID added to Institution class

--- webapp/src/main/java/ar/edu/itba/paw/controllers/DoctorsByInstitutionController.java
+++ webapp/src/main/java/ar/edu/itba/paw/controllers/DoctorsByInstitutionController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,22 +12,38 @@ import ar.edu.itba.paw.Address;
 import ar.edu.itba.paw.Doctor;
 import ar.edu.itba.paw.Institution;
 
-@RequestMapping("/institutions/{institution_id}/doctors")
-@Controller
-public class DoctorsByInstitutionController extends AbstractRESTApiController {
 
-	@Override
-    public Object list() throws MethodNotAllowedException {
-        ModelAndView model = new ModelAndView("doctors_by_institution");
+@Controller
+public class DoctorsByInstitutionController {
+
+	@RequestMapping("/institutions/{institution_id}/doctors")
+    public Object list(@PathVariable final int institution_id) throws MethodNotAllowedException {        
+    	ModelAndView model = new ModelAndView("doctors_by_institution");
         
         List<Doctor> doctors = new ArrayList<Doctor>();
+        Doctor doctor1 = null;
+        Doctor doctor2 = null;
+        Doctor doctor3 = null;
+        Address address;
+        Institution institution = null;
         
-        Doctor doctor1 = new Doctor("Juan", "Perez", "Traumatologo", "juanperez@gmail.com", "juan123");
-        Doctor doctor2 = new Doctor("Carlos", "Lopez", "Cardiologo", "carloslopez@gmail.com", "carlitos");
-        Doctor doctor3 = new Doctor("Pedro", "Garcia", "Neurologo", "pedrogarcia@gmail.com", "123pedrito321");
-        
-        Address address = new Address("Paraguay", 1465, "7°A", "CABA", "Bs.As.", "Argentina");
-        Institution institution = new Institution("Grupo Medico Vertebral", address);
+        if(institution_id==1){
+            doctor1 = new Doctor("Juan", "Perez", "Traumatologo", "juanperez@gmail.com", "juan123");
+            doctor2 = new Doctor("Carlos", "Lopez", "Cardiologo", "carloslopez@gmail.com", "carlitos");
+            doctor3 = new Doctor("Pedro", "Garcia", "Neurologo", "pedrogarcia@gmail.com", "123pedrito321");
+            address = new Address("Paraguay", 1465, "7°A", "CABA", "Bs.As.", "Argentina");
+            institution = new Institution(institution_id,"Grupo Medico Vertebral", address);
+        }
+        else if(institution_id==2){
+            doctor1 = new Doctor("Jose", "Martinez", "Urologo", "josemartinez@gmail.com", "pepemartinez");
+            doctor2 = new Doctor("Matias", "Hernandez", "Pediatra", "matiashernandez@gmail.com", "matihernandez");
+            doctor3 = new Doctor("Felipe", "Marquez", "Otorrinolaringologo", "felipemarquez@gmail.com", "felipeotorrino");        	
+            address = new Address("Cerviño", 3356, "", "CABA", "Bs.As.", "Argentina");
+            institution = new Institution(institution_id,"Hospital Fernandez", address);
+        }
+        else{
+        	model.addObject("wrongId", institution_id);
+        }
 
         doctors.add(doctor1);
         doctors.add(doctor2);
--- webapp/src/main/java/ar/edu/itba/paw/controllers/InstitutionDoctorAppointmentController.java
+++ webapp/src/main/java/ar/edu/itba/paw/controllers/InstitutionDoctorAppointmentController.java
@@ -2,7 +2,6 @@ package ar.edu.itba.paw.controllers;
 
 import ar.edu.itba.paw.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,23 +15,18 @@ import java.util.List;
 /**
  * Created by santi698 on 23/03/16.
  */
-@RequestMapping("/institutions/{institution_id}/doctors/{doctor_id}/appointment_slots")
+
 @Controller
-public class InstitutionDoctorAppointmentController extends AbstractRESTApiController {
+public class InstitutionDoctorAppointmentController {
 
-    @Override
-    public Object create() throws MethodNotAllowedException {
-        return new Object();
-    }
-
-    @Override
-    public Object list() throws MethodNotAllowedException {
+    @RequestMapping("/institutions/{institution_id}/doctors/{doctor_id}/appointment_slots")
+    public Object list(@PathVariable final int institution_id) throws MethodNotAllowedException {
         ModelAndView model = new ModelAndView("institution_doctor_appointment");
         List<Appointment> appointments = new ArrayList<>();
 
         Doctor doctor = new Doctor("Doctor name", "Doctor last name", "Speciality", "Email", "password");
         Address address = new Address("Street Name", 9999, "Apartment", "City", "State", "Country");
-        Institution institution = new Institution("Institution X", address);
+        Institution institution = new Institution(institution_id, "Institution X", address);
 
         for (int i = 0; i < 10; i++) {
             AppointmentSlot slot = new AppointmentSlot(DayOfWeek.of(i % 7 + 1), 20, institution);
@@ -46,19 +40,5 @@ public class InstitutionDoctorAppointmentController extends AbstractRESTApiContr
 
         return model;
     }
-
-    @Override
-    public Object update(@PathVariable Integer id) throws MethodNotAllowedException {
-        return super.update(id);
-    }
-
-    @Override
-    public Object show(@PathVariable Integer id) throws MethodNotAllowedException {
-        return super.show(id);
-    }
-
-    @Override
-    public Object delete(@PathVariable Integer id) throws MethodNotAllowedException {
-        return super.delete(id);
-    }
+    
 }
