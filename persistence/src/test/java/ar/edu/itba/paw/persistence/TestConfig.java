578b0e04d888e7b785c4346f5475178af3d78e87
Joquin Suarez Bodean <jsuarezb@outlook.com>
2016-05-04 13:09:03 -0300

    Moved every sql file to a single file inside persistence resources This will allow adding new sql sentences without changing code. With this change testing DAOs will be easier and less prone to confusion.

---
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/TestConfig.java
@@ -0,0 +1,50 @@
+package ar.edu.itba.paw.persistence;
+
+import org.hsqldb.jdbc.JDBCDriver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+@ComponentScan({"ar.edu.itba.paw.persistence"})
+@Configuration
+class TestConfig {
+
+    private static final String[] schemaFiles = {
+            "sql/schema.sql"
+    };
+
+    @Bean
+    public DataSource dataSource() {
+        final SimpleDriverDataSource ds = new SimpleDriverDataSource();
+        ds.setDriverClass(JDBCDriver.class);
+        ds.setUrl("jdbc:hsqldb:mem:paw");
+        ds.setUsername("paw");
+        ds.setPassword("");
+
+        return ds;
+    }
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(final DataSource ds) {
+        final DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(ds);
+        initializer.setDatabasePopulator(databasePopulator());
+        return initializer;
+    }
+
+    private DatabasePopulator databasePopulator() {
+        final ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        for (String path : schemaFiles)
+            populator.addScripts(new ClassPathResource(path));
+
+        return populator;
+    }
+
+}
--- webapp/src/main/resources/sql/patient_schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS patients
-(
-  id              SERIAL PRIMARY KEY,
-  name            VARCHAR(100) NOT NULL,
-  last_name       VARCHAR(100) NOT NULL,
-  email           VARCHAR(100) NOT NULL,
-  password        VARCHAR(100) NOT NULL
-);
