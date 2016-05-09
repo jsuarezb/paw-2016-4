920ef0f6088d5824d75cb7b21ac6410ab84a9bb5
Joquin Suarez Bodean <jsuarezb@outlook.com>
2016-05-09 17:28:50 -0300

    Speciality combo box in home

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controllers/frontend/IndexController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controllers/frontend/IndexController.java
@@ -1,14 +1,29 @@
 package ar.edu.itba.paw.webapp.controllers.frontend;
 
 
+import ar.edu.itba.paw.models.Speciality;
+import ar.edu.itba.paw.services.SpecialityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class IndexController extends BaseController {
+
+    private static final String SPECIALITIES_KEY = "specialities";
+    @Autowired
+    private SpecialityService specialityService;
+
     @RequestMapping("/")
     public ModelAndView index() {
-        return new ModelAndView("index");
+        ModelAndView mav = new ModelAndView("index");
+
+        List<Speciality> specialities = specialityService.getAll();
+        mav.addObject(SPECIALITIES_KEY, specialities);
+
+        return mav;
     }
 }
--- webapp/src/main/webapp/WEB-INF/templates/index.jsp
+++ webapp/src/main/webapp/WEB-INF/templates/index.jsp
@@ -1,6 +1,20 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
 <%@ taglib prefix="z" tagdir="/WEB-INF/tags" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <z:base title="ChoPido Turnos">
   <div class="jumbotron">
     <h1>ChoPido Turnos</h1>
+    <form action="/grupo4/search" method="GET" class="form-inline">
+        <div class="form-group">
+            <select class="form-control" name="speciality">
+                <c:forEach var="speciality" items="${ specialities }">
+                    <option value="${ speciality.id }">${ speciality.name }</option>
+                </c:forEach>
+            </select>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-primary" type="submit">Buscar</button>
+        </div>
+    </form>
   </div>
 </z:base>
