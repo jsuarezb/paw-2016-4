d3aa16713322d5b99b464d4bae6f0d504a1e5871
Mauricio Minestrelli <mauricio.minestrelli@mercadolibre.com>
2018-02-11 19:28:27 -0300

    - Retrieve appointments dinamically from url param

--- webapp/src/main/frontend/app/scripts/controllers/AppointmentsCtrl.js
+++ webapp/src/main/frontend/app/scripts/controllers/AppointmentsCtrl.js
@@ -1,10 +1,11 @@
 'use strict';
 define(['ChoPidoTurnos', 'services/AppointmentsService'], function(ChoPidoTurnos) {
 
-  ChoPidoTurnos.controller('AppointmentsCtrl', ['AppointmentsService', function(appointmentsService) {
+  ChoPidoTurnos.controller('AppointmentsCtrl', ['AppointmentsService', '$routeParams', function(appointmentsService,$routeParams) {
     var _this = this;
+    this.patientId = $routeParams.patientId;
 
-      appointmentsService.getAppointmentsPatient(5).then(
+      appointmentsService.getAppointmentsPatient(this.patientId).then(
         function (result) {
           console.log(result);
             _this.patientAppointment = result.data;
