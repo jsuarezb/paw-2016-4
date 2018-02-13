e315068a0f7402a24cfe822c60e6f4bfd5c696ef
Joaquin Suarez Bodean <jsuarezb@itba.edu.ar>
2018-02-13 09:49:19 -0300

    Fixes appointment builder exception

--- models/src/main/java/ar/edu/itba/paw/models/errors/InvalidHourException.java
+++ models/src/main/java/ar/edu/itba/paw/models/errors/InvalidHourException.java
@@ -1,8 +1,6 @@
 package ar.edu.itba.paw.models.errors;
 
-import sun.plugin.dom.exception.InvalidStateException;
-
-public class InvalidHourException extends InvalidStateException {
+public class InvalidHourException extends IllegalStateException {
 
     public InvalidHourException() {
         super("La hora debe estar entre 0 y 23");
