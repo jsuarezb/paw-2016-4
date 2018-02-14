952068559da8b709ca51b99e38679469abc317e3
Joaquin Suarez Bodean <jsuarezb@itba.edu.ar>
2018-02-14 02:23:18 -0300

    Group appointments by day

--- webapp/src/main/frontend/app/scripts/controllers/AppointmentSearchCtrl.js
+++ webapp/src/main/frontend/app/scripts/controllers/AppointmentSearchCtrl.js
@@ -9,6 +9,13 @@ define(['ChoPidoTurnos', 'services/appointmentsService'], function(ChoPidoTurnos
       var speciality = $stateParams.speciality || '';
       var neighborhood = $stateParams.neighborhood || '';
 
+      var groupBy = function(xs, f) {
+        return xs.reduce(function(rv, x) {
+          (rv[f(x)] = rv[f(x)] || []).push(x);
+          return rv;
+        }, {});
+      };
+
       appointmentService.searchAppointments(institution, speciality, neighborhood).then(function (data) {
         var page = data.data;
 
@@ -17,11 +24,24 @@ define(['ChoPidoTurnos', 'services/appointmentsService'], function(ChoPidoTurnos
         $scope.emptyAppointments = page.results.length === 0;
         $scope.hasPreviousPage = page.page > 0;
         $scope.hasNextPage = Math.floor(page.total / page.pageSize) > page.page;
-        console.log(data.data);
+
+        if (!$scope.emptyAppointments) {
+          $scope.appointmentGroups = groupBy($scope.appointments, function (app) {
+            var date = new Date(app.date);
+            return new Date(date.getFullYear(), date.getMonth(), date.getDate());
+          });
+
+          $scope.appointmentDays = Object
+            .keys($scope.appointmentGroups)
+            .map(function (d) {
+              return new Date(d);
+            })
+            .sort();
+        }
       });
 
-      $scope.dayOfWeek = function(slot) {
-        switch (slot.dayOfWeek) {
+      $scope.dayOfWeek = function(date) {
+        switch (date.getDay()) {
           case 1: return 'Lunes';
           case 2: return 'Martes';
           case 3: return 'Miércoles';
