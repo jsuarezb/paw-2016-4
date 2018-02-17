fb187212e70bb98d48e84fd2dd2d37abae6f19ca
Joaquin Suarez Bodean <jsuarezb@itba.edu.ar>
2018-02-17 01:52:38 -0300

    Email with metadata for patient

--- services/src/main/java/ar/edu/itba/paw/services/MailServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/MailServiceImpl.java
@@ -1,8 +1,8 @@
 package ar.edu.itba.paw.services;
 
-import ar.edu.itba.paw.models.Appointment;
-import ar.edu.itba.paw.models.Doctor;
-import ar.edu.itba.paw.models.Patient;
+import ar.edu.itba.paw.models.*;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +12,13 @@ import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.velocity.VelocityEngineFactoryBean;
+import org.springframework.ui.velocity.VelocityEngineUtils;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 @Service
@@ -26,8 +30,13 @@ public class MailServiceImpl implements MailService {
 
     private static final DateTimeFormatter timeFmt = DateTimeFormatter.ofPattern("HH:mm");
 
+    private static final DateTimeFormatter isoDateTimeFmt = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss");
+
     @Autowired
     private MailSender mailSender;
+
+    @Autowired
+    private VelocityEngine velocityEngine;
 
     @Override
     public void sendAppointmentConfirmationToDoctor(final Appointment appointment, final Doctor doctor,
@@ -53,20 +62,44 @@ public class MailServiceImpl implements MailService {
     public void sendAppointmentConfirmationToPatient(final Appointment appointment,
                                                      final Doctor doctor,
                                                      final Patient patient) {
-        final SimpleMailMessage msg = new SimpleMailMessage();
+        final VelocityEngine ve = new VelocityEngine();
+        ve.init();
+
         final LocalDateTime date = appointment.getDate();
-        msg.setFrom("chopidoturnos@gamil.com");
-        msg.setSubject("Turno reservado.");
-        msg.setTo(patient.getEmail());
-        msg.setText("Usted reservó un turno para el día " + date.format(dateFmt)
-                + " a las " + date.format(timeFmt) + " hs"
-                + String.format("%s, %s", doctor.getLastName(), doctor.getName())
-        );
+        final Institution institution = appointment.getSlot().getWorksIn().getInstitution();
+        final Address institutionAddress = institution.getAddress();
+
+        final Map<String, Object> context = new HashMap<>();
+
+        context.put("doctorName", String.format("%s %s", doctor.getName(), doctor.getLastName()));
+        context.put("appointmentDate", date.format(dateFmt));
+        context.put("appointmentTime", date.format(timeFmt));
+        context.put("institutionName", institution.getName());
+        context.put("institutionStreetNumber", institutionAddress.getStreetNumber());
+        context.put("institutionStreetName", institutionAddress.getStreetName());
+        context.put("institutionNeighborhood", institutionAddress.getNeighborhood());
+        context.put("institutionCity", institutionAddress.getCity());
+        context.put("institutionState", institutionAddress.getState());
+        context.put("institutionCountry", institutionAddress.getCountry());
+        context.put("appointmentStartDatetime", date.format(isoDateTimeFmt));
+        context.put("appointmentEndDatetime", date.plusHours(1).format(isoDateTimeFmt));
 
         try {
+            final String emailText = VelocityEngineUtils.mergeTemplateIntoString(
+                    velocityEngine,
+                    "templates/patient_booked_appointment.vm",
+                    "UTF-8",
+                    context);
+
+            final SimpleMailMessage msg = new SimpleMailMessage();
+            msg.setFrom("chopidoturnos@gamil.com");
+            msg.setSubject("Turno reservado.");
+            msg.setTo(patient.getEmail());
+            msg.setText(emailText);
+
             mailSender.send(msg);
-        } catch (MailException ex) {
-            // TODO log error
+        } catch (final MailException ex) {
+            ex.printStackTrace();
         }
     }
 
@@ -108,7 +141,7 @@ public class MailServiceImpl implements MailService {
         try {
             mailSender.send(msg);
         } catch (MailException ex) {
-            // TODO log error
+            LOG.error("Mail not sent.", ex);
         }
     }
 
@@ -128,5 +161,18 @@ public class MailServiceImpl implements MailService {
         mailSender.setJavaMailProperties(properties);
 
         return mailSender;
+    }
+
+    @Bean
+    private static VelocityEngineFactoryBean velocityEngineFactoryBean() {
+        final VelocityEngineFactoryBean engineFactory = new VelocityEngineFactoryBean();
+        final Map<String, Object> velocityProperties = new HashMap<>();
+
+        velocityProperties.put("resource.loader", "class");
+        velocityProperties.put("class.resource.loader.class", ClasspathResourceLoader.class.getCanonicalName());
+
+        engineFactory.setVelocityPropertiesMap(velocityProperties);
+
+        return engineFactory;
     }
 }
