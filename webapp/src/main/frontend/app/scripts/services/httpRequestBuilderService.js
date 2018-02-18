183d1cb965a46fa119af5776f4348a7e7b9fe410
Agop Hurmuz <agophurmuz@gmail.com>
2018-02-17 23:12:28 -0300

    fixed httpRequestBuilderService and started date picker

--- webapp/src/main/frontend/app/scripts/services/httpRequestBuilderService.js
+++ webapp/src/main/frontend/app/scripts/services/httpRequestBuilderService.js
@@ -1,29 +1,32 @@
 'use strict';
-angular.module('ChoPidoTurnos').factory('httpRequestBuilderService',
-  ['$http', function($http) {
-    return {
-      buildHttpRequest: function (requestMethod, requestUrl, requestData) {
-        return $http({
-          method: requestMethod,
-          // change this to general
-          url: '/grupo4/api/v1/' + requestUrl,
-          headers: {
-            'content-type': 'application/json'
-          },
-          data: requestData
-        });
-      },
-      buildAuthenticatedHttpRequest: function (requestMethod, requestUrl, requestData, token) {
-        return $http({
-          method: requestMethod,
-          // change this to general
-          url: '/grupo4/api/v1/' + requestUrl,
-          headers: {
-            'content-type': 'application/json',
-            'authentication': token
-          },
-          data: null
-        });
-      }
-    };
-  }]);
+define(['ChoPidoTurnos'], function(ChoPidoTurnos) {
+  ChoPidoTurnos.factory('httpRequestBuilderService',
+    ['$http', function($http) {
+      return {
+        buildHttpRequest: function (requestMethod, requestUrl, requestData) {
+          return $http({
+            method: requestMethod,
+            // change this to general
+            url: '/grupo4/api/v1/' + requestUrl,
+            headers: {
+              'content-type': 'application/json'
+            },
+            data: requestData
+          });
+        },
+        buildAuthenticatedHttpRequest: function (requestMethod, requestUrl, requestData, token) {
+          return $http({
+            method: requestMethod,
+            // change this to general
+            url: '/grupo4/api/v1/' + requestUrl,
+            headers: {
+              'content-type': 'application/json',
+              'authentication': token
+            },
+            data: null
+          });
+        }
+      };
+    }]
+  );
+});
