c8ebb5781f2cf1ed6c19839162e45b9c2a902b5d
Santiago Ocamica <santi6982@gmail.com>
2018-02-18 22:49:39 -0300

    Makes doctors be able to be patients too

--- webapp/src/main/java/ar/edu/itba/paw/webapp/auth/Token.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/auth/Token.java
@@ -1,10 +1,14 @@
 package ar.edu.itba.paw.webapp.auth;
 
 import ar.edu.itba.paw.models.User;
+import ar.edu.itba.paw.models.Doctor;
+import ar.edu.itba.paw.models.Patient;
 import ar.edu.itba.paw.webapp.config.WebAuthConfig;
 import io.jsonwebtoken.*;
 
 import java.util.Date;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Created by alebian on 12/8/16.
@@ -13,12 +17,21 @@ public class Token {
     private final static Long TOKEN_DURATION = 86400000L; // 1 Day
 
     public static String create(final User user) {
+        final Map<String, Object> claims = new HashMap<>();
+        final Doctor doctor = user.getDoctor();
+        final Patient patient = user.getPatient();
+        if (doctor != null) {
+            claims.put("doctorId", doctor.getId());
+        }
+        if (patient != null) {
+            claims.put("patientId", patient.getId());
+        }
         return Jwts.builder()
                 .signWith(SignatureAlgorithm.HS512, WebAuthConfig.APP_KEY)
+                .setClaims(claims)
                 .setSubject(user.getId().toString())
                 .setIssuedAt(currentDate())
                 .setExpiration(expirationDate())
-                .setId(user.type())
                 .setIssuer(user.getEmail())
                 .compact();
     }
